0bf6884fea1c0537bfc1fd48e3b0a442715a5e40
ouob <kyjin320@sookmyung.ac.kr>
2024-02-19 15:27:56 +0900

    fix: 기록하기 viewpager2 수정

--- app/src/main/java/com/remine/ui/home/ReportAdapter.kt
+++ app/src/main/java/com/remine/ui/home/ReportAdapter.kt
@@ -2,18 +2,28 @@ package com.remine.ui.home
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ReportAdapter (manager: FragmentManager): FragmentPagerAdapter(manager) {
+class ReportAdapter (
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
 
     private val fragmentList = ArrayList<Fragment>()
     private val titleList = ArrayList<String>()
 
-    override fun getItem(position: Int): Fragment = fragmentList[position]
+    /*override fun getItem(position: Int): Fragment = fragmentList[position]
 
     override fun getCount(): Int = titleList.size
 
-    override fun getPageTitle(position: Int): CharSequence = titleList[position]
+    override fun getPageTitle(position: Int): CharSequence = titleList[position]*/
+
+    override fun createFragment(position: Int): Fragment = fragmentList[position]
+
+    override fun getItemCount(): Int = titleList.size
+
+    fun getPageTitle(position: Int): CharSequence = titleList[position]
 
     fun addFragment(fragment: Fragment, title: String)
     {
