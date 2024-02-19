a4de5bff851b203a5b26d7b137f3c460d4e292eb
alotofhee <ekgmljeong@gmail.com>
2024-02-19 16:11:30 +0900

    fix: 선언 data class 수정

--- app/src/main/java/com/remine/ui/declaration/Declaration.kt
+++ app/src/main/java/com/remine/ui/declaration/Declaration.kt
@@ -1,6 +1,14 @@
 package com.remine.ui.declaration
 
+import com.google.gson.annotations.SerializedName
+
 data class Declaration(
+    @SerializedName("declarationId")
+    val declarationId: Int,
+    @SerializedName("createdDate")
     val date: String,
-    val declaration: String
+    @SerializedName("content")
+    val declaration: String,
+    @SerializedName("audioFileURL")
+    val voice: String
 )
